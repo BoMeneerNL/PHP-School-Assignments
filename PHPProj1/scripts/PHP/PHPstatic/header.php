37d605c5d1d1801d0adb137eaa0792867d66db2a
BoMeneerNL <51164986+BoMeneerNL@users.noreply.github.com>
2021-04-11 15:48:40 +0200

    minified js & css files

--- PHPProj1/scripts/PHP/PHPstatic/header.php
+++ PHPProj1/scripts/PHP/PHPstatic/header.php
@@ -2,4 +2,4 @@
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="scripts/css/tailwind.css">
+    <link rel="stylesheet" href="scripts/css/min.tailwind.css">
--- PHPProj1/scripts/PHP/PHPstatic/scriptimport.php
+++ PHPProj1/scripts/PHP/PHPstatic/scriptimport.php
@@ -1,2 +1,2 @@
 <script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"></script>
-<script src = "scripts/js/main.js"></script>
+<script src = "scripts/js/min.main.js"></script>
